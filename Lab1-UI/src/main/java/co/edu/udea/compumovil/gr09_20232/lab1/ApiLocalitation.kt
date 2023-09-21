9e5dc10bf3a330962af735a2637d81b6c90f9e34
laura tascon <laura.tascon@udea.edu.co>
2023-09-20 20:13:52 -0500

    Separación de los componentes que antes 	estaban juntos en secondForm para la aplicación 	más ordenada y escalable. 	También se cambio la API de localización que se estaba 	consumiendo por un mock, hasta el momento solo se consume 	el mock de paises sudamericanos.

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20232/lab1/ApiLocalitation.kt
@@ -0,0 +1,50 @@
+package co.edu.udea.compumovil.gr09_20232.lab1
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+data class CountryData(
+    @SerializedName("country_name") val countryName: String,
+)
+
+data class StatesData(
+    @SerializedName("state_name") val stateName: String,
+)
+
+data class CitiesData(
+    @SerializedName("city_name") val cityName: String,
+)
+
+interface LocalitationApi {
+
+    @GET("countries")
+    suspend fun getAllCountries(): List<CountryData>
+
+    @GET("states")
+    suspend fun getAllStates(): List<StatesData>
+
+    @GET("states/{countryName}")
+    suspend fun getStateByCountry(@Path("countryName") countryName: String): List<StatesData>
+
+    @GET("cities")
+    suspend fun getAllCities(): List<CitiesData>
+
+    @GET("cities/{stateName}")
+    suspend fun getCitiesByName(@Path("stateName") citiesName: String): List<CitiesData>
+
+
+}
+
+
+object LocalitationApiService {
+    private const val BASE_URL = "https://private-63af11-lauravanessatascon.apiary-mock.com/"
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val localitationApi: LocalitationApi = retrofit.create(LocalitationApi::class.java)
+}
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20232/lab1/SecondForm.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20232/lab1/SecondForm.kt
@@ -51,43 +51,7 @@ class ContactDataActivity : ComponentActivity() {
     }
 }
 
-data class CountryData(
-    @SerializedName("country_name") val countryName: String,
-    @SerializedName("country_short_name") val countryShortName: String,
-    @SerializedName("country_phone_code") val countryPhoneCode: Int
-)
 
-data class StatesData(
-    @SerializedName("state_name") val stateName: String,
-)
-
-data class CitiesData(
-    @SerializedName("city_name") val cityName: String,
-)
-
-interface LocalitationApi {
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJsYXVyYS50YXNjb25AdWRlYS5lZHUuY28iLCJhcGlfdG9rZW4iOiJUVlZhTTZCMFdWRFppcllpeDUzNmd6bFdUTm1FYV9HMmtxY2laeDE1emtFV0hqNWk3SXM5OGg3MFFtVTJobUJoN2RBIn0sImV4cCI6MTY5NTEzMzUxM30.y_hLzfHNLxW4gLox8WEjgjIcqWYghk_t1Yho5U-alIw",
-        "Accept: application/json"
-    )
-
-    @GET("countries")
-    suspend fun getAllCountries(): List<CountryData>
-
-    @GET("states")
-    suspend fun getAllStates(): List<StatesData>
-
-    @GET("states/{countryName}")
-    suspend fun getStateByCountry(@Path("countryName") countryName: String): List<StatesData>
-
-    @GET("cities")
-    suspend fun getAllCities(): List<CitiesData>
-
-    @GET("cities/{stateName}")
-    suspend fun getCitiesByName(@Path("stateName") citiesName: String): List<CitiesData>
-
-
-}
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
@@ -98,168 +62,15 @@ fun SecondMainContent(){
         HeaderWithTitle(title = "Personal Information")
         FormElement(label = "Teléfono", inputType = InputType.Number)
         FormElement(label = "Correo", inputType = InputType.Email)
-        dropDownMenu(selectItem = "selecciona tu país")
+        CountryDownMenu(selectItem = "selecciona tu país")
         FormElement(label = "Dirección", inputType = InputType.Number)
     }
 
 }
 
-object LocalitationApiService {
-    private const val BASE_URL = "https://www.universal-tutorial.com/api/"
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val localitationApi: LocalitationApi = retrofit.create(LocalitationApi::class.java)
-}
 
 
-@ExperimentalMaterial3Api
-@Composable
-fun dropDownMenu(selectItem: String){
-    var selectedItem by remember { mutableStateOf(" ")  }
-    var countries: List<CountryData>? by remember { mutableStateOf(null) }
 
-
-    var expanded by remember { mutableStateOf(false) }
-
-    var textFieldSize by remember { mutableStateOf(Size.Zero) }
-
-    val icon = if (expanded){
-        Icons.Filled.KeyboardArrowUp
-    }else{
-        Icons.Filled.KeyboardArrowDown
-    }
-
-    var hasLoadedDataCountry by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        if (!hasLoadedDataCountry) {
-            withContext( context = Dispatchers.IO) {
-                try {
-
-                    val loadedCountries = LocalitationApiService.localitationApi.getAllCountries()
-                    countries = loadedCountries
-                    hasLoadedDataCountry = true
-                    Log.d("API Response","Prueba de log")
-                } catch (e: Exception) {
-                    Log.e("API Error", e.message ?: "Unknown error")
-                }
-            }
-        }
-    }
-
-
-    Column (modifier = Modifier.padding(20.dp)){
-
-        OutlinedTextField(
-            value = selectedItem,
-            onValueChange = {selectedItem = it},
-            readOnly = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    textFieldSize = coordinates.size.toSize()
-                },
-            label = { Text(text = selectItem)},
-            textStyle = LocalTextStyle.current.copy(
-                color = LocalContentColor.current
-            ),
-            trailingIcon = {
-                Icon(icon, "", Modifier
-                    .clickable { expanded = !expanded })
-            }
-        )
-
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false},
-            modifier = Modifier
-                .width(with(LocalDensity.current){textFieldSize.width.toDp()})
-        ) {
-            //Log.d("API", "Lista: "+countries.toString())
-            countries?.forEach { label ->
-                DropdownMenuItem(text = { Text(text = label.countryName) }, onClick = {
-                    selectedItem = label.countryName
-                    expanded = false
-                })
-            }
-
-        }
-    }
-    Log.d("pais seleccionado",selectedItem)
-
-    //otro
-    var states: List<StatesData>? by remember { mutableStateOf(null) }
-
-    var selectedItem2 by remember { mutableStateOf(" ")  }
-    var expanded2 by remember { mutableStateOf(false) }
-    val icon2 = if (expanded2){
-        Icons.Filled.KeyboardArrowUp
-    }else{
-        Icons.Filled.KeyboardArrowDown
-    }
-
-    var hasLoadedDataState by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        Log.d("pais seleccionado mor",selectedItem)
-
-        if (!hasLoadedDataState) {
-            withContext( context = Dispatchers.IO) {
-                try {
-
-                    val loadedStates = LocalitationApiService.localitationApi.getStateByCountry("Colombia")
-                    states = loadedStates
-                    hasLoadedDataState = true
-                    Log.d("API Response","Prueba de log")
-                } catch (e: Exception) {
-                    Log.e("API Error", e.message ?: "Unknown error")
-                }
-            }
-        }
-    }
-
-
-    Column (modifier = Modifier.padding(20.dp)){
-
-        OutlinedTextField(
-            value = selectedItem2,
-            onValueChange = {selectedItem2 = it},
-            readOnly = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    textFieldSize = coordinates.size.toSize()
-                },
-            label = { Text(text = selectItem)},
-            textStyle = LocalTextStyle.current.copy(
-                color = LocalContentColor.current
-            ),
-            trailingIcon = {
-                Icon(icon2, "", Modifier
-                    .clickable { expanded2 = !expanded2 })
-            }
-        )
-
-        DropdownMenu(
-            expanded = expanded2,
-            onDismissRequest = { expanded2 = false},
-            modifier = Modifier
-                .width(with(LocalDensity.current){textFieldSize.width.toDp()})
-        ) {
-            //Log.d("API", "Lista: "+countries.toString())
-            states?.forEach { label ->
-                DropdownMenuItem(text = { Text(text = label.stateName) }, onClick = {
-                    selectedItem2 = label.stateName
-                    expanded2 = false
-                })
-            }
-
-        }
-    }
-}
 
 
 
